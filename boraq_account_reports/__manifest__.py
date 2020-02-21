e5d02c8b65e68559aabb184c872b8535b531ad07
101t <tarek.it.eng@gmail.com>
2020-02-21 16:48:43 +0300

    initial module

---
+++ boraq_account_reports/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Boraq Account Report",
+
+    'summary': """
+        This module will help to add analytic filter in report.
+        
+        """,
+
+    'description': """
+        This module will help to add analytic filter in report.
+    """,
+
+    'author': "Boraq-Group",
+    'website': "",
+    'category': 'Accounting',
+    'version': '12.0',
+    'depends': ['account_reports','account'],
+    'data': [
+        'views/account_financial_report_data.xml',
+        ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+}
